85ab9af0d0ea47cb56e171f04b1b3c8eed15f118
Joe Pantazidis <joe.pantazidis@outlook.de>
2020-11-29 13:28:35 +0100

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  extends: [
+    '@nuxtjs',
+    'plugin:nuxt/recommended',
+    'airbnb-base',
+  ],
+  plugins: [
+  ],
+  // add your custom rules here
+  rules: {
+    'max-len': ['error', { code: 120 }],
+    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+  },
+};
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable global-require */
+
+module.exports = {
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      'components/**/*.vue',
+      'layouts/**/*.vue',
+      'pages/**/*.vue',
+      'plugins/**/*.js',
+      'nuxt.config.js',
+    ],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('tailwindcss-children'),
+  ],
+};
