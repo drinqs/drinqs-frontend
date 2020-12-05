4d2729b598333c20e69fdc6a4a9deb75c51ba392
Joe Pantazidis <joe.pantazidis@outlook.de>
2020-12-05 09:21:50 +0100

    add @nuxtjs/html-validator

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,6 +5,9 @@ export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     title: 'Drinqs',
+    htmlAttrs: {
+      lang: 'en',
+    },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -41,6 +44,7 @@ export default {
     '@nuxtjs/eslint-module',
     // https://go.nuxtjs.dev/tailwindcss
     '@nuxtjs/tailwindcss',
+    '@nuxtjs/html-validator',
   ],
 
   storybook: {
