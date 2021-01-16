bbc97203bba608b923c919d319ca27c6f0cb18d5
Joe Pantazidis <joe.pantazidis@outlook.de>
2021-01-16 11:13:29 +0100

    add forgot-password page (and ignore it for now)

---
+++ components/User/ForgotPasswordForm.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="bg-gray-50 p-4 rounded-lg">
+    <form @submit.prevent="onSubmit">
+      <div class="field">
+        <BaseInput
+          id="username-or-email"
+          :value="userData.usernameOrEmail"
+          label="Username / Email"
+          @input="updateUserData('usernameOrEmail', $event)"
+        />
+      </div>
+
+      <div class="actions">
+        <button
+          :disabled="loading"
+          type="submit"
+          class="button button-primary w-full inline-flex items-center justify-center mb-2"
+        >
+          <Spinner v-show="loading" class="animate-spin -ml-8 mr-3 h-5 w-5" />
+          Reset Password
+        </button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ForgotPasswordForm',
+  model: {
+    prop: 'userData',
+    event: 'change',
+  },
+  props: {
+    userData: {
+      type: Object,
+      required: true,
+    },
+    loading: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  created() {
+    this.$emit('change', {
+      usernameOrEmail: '',
+    });
+  },
+  methods: {
+    updateUserData(key, value) {
+      this.$emit('change', {
+        ...this.userData,
+        [key]: value,
+      });
+    },
+    onSubmit() {
+      this.$emit('submit');
+    },
+  },
+};
+</script>
---
+++ pages/forgot-password.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="max-w-lg mx-auto">
+    <h1 class="text-secondary font-bold text-xl mb-4">
+      Forgot Your Password?
+    </h1>
+
+    <ForgotPasswordForm
+      v-model="userData"
+      :loading="loading"
+      @submit="onForgotPassword"
+    />
+
+    <div class="mt-2 p-4">
+      <p class="text-secondary text-base text-center">
+        Remembered Your Password?
+      </p>
+
+      <NuxtLink
+        to="/login"
+        role="button"
+        class="button button-secondary w-full inline-flex justify-center mt-2"
+      >
+        Login
+      </NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+
+export default {
+  middleware: 'guest',
+  data() {
+    return {
+      userData: {},
+      loading: false,
+    };
+  },
+  methods: {
+    ...mapActions('flash', ['setFlashMessage']),
+    async forgotPassword() {
+      // TODO
+    },
+
+    async onForgotPassword() {
+      this.loading = true;
+
+      try {
+        await this.forgotPassword(this.userData);
+
+        this.setFlashMessage({
+          type: 'success',
+          message: 'You will receive instructions to reset your password. Check your mails!',
+          shortMessage: 'You will receive reset instructions.',
+        });
+
+        this.$router.push({ path: '/login' });
+      } catch (err) {
+        this.$notification.error({
+          message: 'Oops! Something went wrong.',
+          shortMessage: 'Something went wrong.',
+        });
+      }
+
+      this.loading = false;
+    },
+  },
+};
+</script>
