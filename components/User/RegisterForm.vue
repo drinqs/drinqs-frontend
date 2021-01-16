562b5485d6b0c462f7daa08d6d0aba7aa1fdfc7f
Joe Pantazidis <joe.pantazidis@outlook.de>
2021-01-16 13:28:20 +0100

    validate register form

--- components/User/RegisterForm.vue
+++ components/User/RegisterForm.vue
@@ -5,6 +5,7 @@
         <BaseInput
           id="username"
           :value="userData.username"
+          :error="getError('username')"
           label="Username"
           @input="updateUserData('username', $event)"
         />
@@ -14,7 +15,9 @@
         <BaseInput
           id="email"
           :value="userData.email"
+          :error="getError('email')"
           label="Email"
+          type="email"
           @input="updateUserData('email', $event)"
         />
       </div>
@@ -41,6 +44,7 @@
         <PasswordInput
           id="password"
           :value="userData.password"
+          :error="getError('password')"
           label="Password"
           @input="updateUserData('password', $event)"
         />
@@ -50,6 +54,7 @@
         <PasswordInput
           id="password-confirmation"
           :value="userData.passwordConfirmation"
+          :error="getError('passwordConfirmation')"
           label="Password Confirmation"
           @input="updateUserData('passwordConfirmation', $event)"
         />
@@ -57,7 +62,7 @@
 
       <div class="actions">
         <button
-          :disabled="loading"
+          :disabled="loading || ($v.userData.$invalid && $v.userData.$anyDirty)"
           type="submit"
           class="button button-primary w-full inline-flex items-center justify-center mb-2"
         >
@@ -70,6 +75,8 @@
 </template>
 
 <script>
+import { required, email, minLength, sameAs } from 'vuelidate/lib/validators';
+
 export default {
   name: 'RegisterForm',
   model: {
@@ -86,14 +93,28 @@ export default {
       default: false,
     },
   },
-  created() {
-    this.$emit('change', {
-      username: '',
-      email: '',
-      birthday: '',
-      password: '',
-      passwordConfirmation: '',
-    });
+  validations: {
+    userData: {
+      username: {
+        required,
+        minLength: minLength(3),
+      },
+      email: {
+        required,
+        email,
+      },
+      password: {
+        required,
+        minLength: minLength(6),
+      },
+      passwordConfirmation: {
+        required,
+        // eslint-disable-next-line func-names
+        sameAsPassword: sameAs(function () {
+          return this.userData.password;
+        }),
+      },
+    },
   },
   methods: {
     updateUserData(key, value) {
@@ -101,9 +122,53 @@ export default {
         ...this.userData,
         [key]: value,
       });
+
+      if (!this.validated) return;
+
+      this.$nextTick(() => {
+        this.$v.userData[key].$touch();
+      });
     },
+
+    getError(key) {
+      if (key === 'username' && this.$v.userData.username.$dirty) {
+        if (!this.$v.userData.username.required) return 'Username is required';
+        if (!this.$v.userData.username.minLength) {
+          return `Username must have at least ${this.$v.userData.username.$params.minLength.min} letters`;
+        }
+        return '';
+      }
+
+      if (key === 'email' && this.$v.userData.email.$dirty) {
+        if (!this.$v.userData.email.required) return 'Email is required';
+        if (!this.$v.userData.email.email) return 'Email must be a valid email address';
+        return '';
+      }
+
+      if (key === 'password' && this.$v.userData.password.$dirty) {
+        if (!this.$v.userData.password.required) return 'Password is required';
+        if (!this.$v.userData.password.minLength) {
+          return `Password must have at least ${this.$v.userData.password.$params.minLength.min} letters`;
+        }
+        return '';
+      }
+
+      if (key === 'passwordConfirmation' && this.$v.userData.passwordConfirmation.$dirty) {
+        if (!this.$v.userData.passwordConfirmation.required) return 'Password confirmation is required';
+        if (!this.$v.userData.passwordConfirmation.sameAsPassword) return 'Password do not match';
+        return '';
+      }
+
+      return '';
+    },
+
     onSubmit() {
-      this.$emit('register');
+      this.$v.$touch();
+      if (this.$v.$invalid) {
+        this.$notification.error('Oops! There are some errors in your inputs.');
+      } else {
+        this.$emit('register');
+      }
     },
   },
 };
