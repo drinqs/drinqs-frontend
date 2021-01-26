f9dce1917d33a37d350f91175492e6cf8ed7ecda
Joe Pantazidis <joe.pantazidis@outlook.de>
2021-01-26 21:23:42 +0100

    re-enable index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,16 @@
 <template>
-  <div class="container">
-    Hello World!
+  <div
+    class="max-w-lg mx-auto w-full flex items-center justify-center"
+    :style="{ 'min-height': 'calc(100vh - 10rem)' }"
+  >
+    <h1 class="text-secondary font-bold text-4xl min-w-full text-center">
+      Welcome to drinqs!
+    </h1>
   </div>
 </template>
 
 <script>
 export default {
   auth: false,
-  middleware({ redirect }) {
-    redirect('/start');
-  },
 };
 </script>
