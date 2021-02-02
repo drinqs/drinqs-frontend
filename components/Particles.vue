587283ad606c9cd16c6b650b178fa519eed54333
Tobias Hoge <tbho@tbho.de>
2021-02-02 17:12:36 +0100

    add particles to index page

---
+++ components/Particles.vue
@@ -0,0 +1,98 @@
+<template>
+  <div id="particle-container">
+    <div class="particle" />
+    <div class="particle" />
+    <div class="particle" />
+    <div class="particle" />
+    <div class="particle" />
+    <div class="particle" />
+    <div class="particle" />
+    <div class="particle" />
+    <div class="particle" />
+    <div class="particle" />
+    <div class="particle" />
+    <div class="particle" />
+    <div class="particle" />
+    <div class="particle" />
+    <div class="particle" />
+    <div class="particle" />
+    <div class="particle" />
+    <div class="particle" />
+    <div class="particle" />
+    <div class="particle" />
+    <div class="particle" />
+    <div class="particle" />
+    <div class="particle" />
+    <div class="particle" />
+    <div class="particle" />
+    <div class="particle" />
+    <div class="particle" />
+    <div class="particle" />
+    <div class="particle" />
+    <div class="particle" />
+    <div class="particle" />
+    <div class="particle" />
+    <div class="particle" />
+    <div class="particle" />
+    <div class="particle" />
+    <div class="particle" />
+    <div class="particle" />
+    <div class="particle" />
+    <div class="particle" />
+    <div class="particle" />
+    <div class="particle" />
+    <div class="particle" />
+    <div class="particle" />
+    <div class="particle" />
+    <div class="particle" />
+    <div class="particle" />
+    <div class="particle" />
+    <div class="particle" />
+    <div class="particle" />
+    <div class="particle" />
+    <div class="particle" />
+    <div class="particle" />
+    <div class="particle" />
+    <div class="particle" />
+    <div class="particle" />
+    <div class="particle" />
+    <div class="particle" />
+    <div class="particle" />
+    <div class="particle" />
+    <div class="particle" />
+  </div>
+</template>
+
+<style lang="scss">
+.particle {
+  position: absolute;
+  border-radius: 50%;
+}
+
+@for $i from 1 through 60 {
+  @keyframes particle-animation-#{$i} {
+    100% {
+      transform: translate3d(
+        (random(90) * 1vw),
+        (random(90) * 1vh),
+        (random(100) * 1px)
+      );
+    }
+  }
+
+  .particle:nth-child(#{$i}) {
+    animation: particle-animation-#{$i} 15s infinite;
+    $size: random(5) + 5 + px;
+    opacity: random(100) / 50;
+    height: $size;
+    width: $size;
+    animation-delay: -$i * 0.2s;
+    transform: translate3d(
+      (random(90) * 1vw),
+      (random(90) * 1vh),
+      (random(100) * 1px)
+    );
+    background: hsl(random(360), 70%, 50%);
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,14 @@
 <template>
-  <div
-    class="max-w-lg mx-auto w-full flex items-center justify-center"
-    :style="{ 'min-height': 'calc(100vh - 10rem)' }"
-  >
-    <h1 class="text-secondary font-bold text-4xl min-w-full text-center">
-      Welcome to drinqs!
-    </h1>
+  <div>
+    <Particles />
+    <div
+      class="max-w-lg mx-auto w-full flex items-center justify-center"
+      :style="{ 'min-height': 'calc(100vh - 10rem)' }"
+    >
+      <h1 class="text-secondary font-bold text-4xl min-w-full text-center">
+        Welcome to drinqs!
+      </h1>
+    </div>
   </div>
 </template>
 
