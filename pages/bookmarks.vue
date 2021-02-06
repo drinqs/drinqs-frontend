d46c85bd841e3f5dfe576c665848bc21c2dcd1d1
Joe Pantazidis <joe.pantazidis@outlook.de>
2021-02-06 17:07:45 +0100

    rework bookmarks page

--- pages/bookmarks.vue
+++ pages/bookmarks.vue
@@ -1,66 +1,101 @@
 <template>
   <div class="max-w-lg mx-auto w-full">
-    <h1 class="text-secondary font-bold text-xl mb-4">
-      Your Bookmarks
-    </h1>
+    <nav class="w-full flex mb-4">
+      <button
+        v-for="tab in tabs"
+        :key="tab.key"
+        :class="tabCssClasses(tab)"
+        @click="switchTab(tab)"
+      >
+        {{ tab.name }}
+      </button>
+    </nav>
 
     <CocktailTile
       v-for="(cocktail, index) in bookmarks"
-      :key="cocktail.slug"
+      v-show="currentTab.key === 'bookmarks'"
+      :key="`bookmark-${cocktail.slug}`"
       :cocktail="cocktail"
       class="mb-8"
-      @remove-bookmark="onRemoveBookmark(index)"
+      @action="onAction({ index, ...$event })"
+    />
+
+    <CocktailTile
+      v-for="(cocktail, index) in likedCocktails"
+      v-show="currentTab.key === 'likedCocktails'"
+      :key="`like-${cocktail.slug}`"
+      :cocktail="cocktail"
+      class="mb-8"
+      @action="onAction({ index, ...$event })"
+    />
+
+    <CocktailTile
+      v-for="(cocktail, index) in dislikedCocktails"
+      v-show="currentTab.key === 'dislikedCocktails'"
+      :key="`dislike-${cocktail.slug}`"
+      :cocktail="cocktail"
+      class="mb-8"
+      @action="onAction({ index, ...$event })"
     />
 
     <Spinner
-      v-show="$fetchState.pending"
+      v-show="loading"
       class="text-secondary h-12 w-12 mx-auto"
     />
   </div>
 </template>
 
 <script>
-import BookmarksQuery from '@/graphql/queries/User/Bookmarks.gql';
+import { get, call } from 'vuex-pathify';
 
 export default {
-  data() {
-    return {
-      bookmarks: [],
-      endCursor: null,
-      nextPageAvailable: true,
-    };
-  },
-  async fetch() {
-    const variables = { first: 10 };
-    if (this.endCursor) variables.after = this.endCursor;
-
-    const { data } = await this.$apolloProvider.defaultClient.query({
-      query: BookmarksQuery,
-      variables,
-    });
-
-    this.endCursor = data.bookmarks.pageInfo.endCursor;
-    this.nextPageAvailable = data.bookmarks.pageInfo.hasNextPage;
-
-    this.bookmarks.push(...data.bookmarks.cocktails);
+  computed: {
+    ...get('navigation', ['isBackNavigation']),
+    ...get('bookmarks', ['tabs', 'currentTab']),
+    loading: get('bookmarks/currentMeta@loading'),
+    bookmarks: get('bookmarks/cocktails@bookmarks'),
+    likedCocktails: get('bookmarks/cocktails@likedCocktails'),
+    dislikedCocktails: get('bookmarks/cocktails@dislikedCocktails'),
   },
   mounted() {
     window.addEventListener('scroll', this.onScroll);
+    if (!this.isBackNavigation) {
+      this.reset();
+      window.scrollTo(0, 0);
+      this.fetch();
+    }
   },
   beforeDestroy() {
     window.removeEventListener('scroll', this.onScroll);
   },
   methods: {
+    ...call('bookmarks', ['reset', 'fetch', 'switchTab', 'onAction']),
+
+    tabCssClasses(tab) {
+      const isCurrentTab = this.currentTab.key === tab.key;
+
+      return [
+        'w-1/3', 'p-4', 'pt-0', 'block', 'focus:outline-none', 'text-sm',
+        {
+          'font-semibold': isCurrentTab,
+          'text-gray-600': !isCurrentTab,
+          'hover:text-secondary': !isCurrentTab,
+          'text-secondary': isCurrentTab,
+          'hover:text-secondary-dark': isCurrentTab,
+
+          'border-b-2': isCurrentTab,
+          'border-secondary': isCurrentTab,
+          'border-b': !isCurrentTab,
+          'border-gray-300': !isCurrentTab,
+        },
+      ];
+    },
+
     onScroll() {
       // if bottom of page reached
       if (document.documentElement.scrollTop + window.innerHeight === document.documentElement.offsetHeight) {
-        if (this.nextPageAvailable) this.$fetch();
+        this.fetch();
       }
-    },
-
-    onRemoveBookmark(cocktailIndex) {
-      // remove cocktail from list if no longer bookmarked
-      this.bookmarks.splice(cocktailIndex, 1);
     },
   },
 };
---
+++ store/bookmarks.js
@@ -0,0 +1,209 @@
+/* eslint-disable no-param-reassign */
+import { make } from 'vuex-pathify';
+import BookmarksQuery from '@/graphql/queries/User/Bookmarks.gql';
+import LikedCocktailsQuery from '@/graphql/queries/User/LikedCocktails.gql';
+import DislikedCocktailsQuery from '@/graphql/queries/User/DislikedCocktails.gql';
+
+const TABS = [
+  {
+    key: 'bookmarks',
+    name: 'Bookmarks',
+  },
+  {
+    key: 'likedCocktails',
+    name: 'Likes',
+  },
+  {
+    key: 'dislikedCocktails',
+    name: 'Dislikes',
+  },
+];
+const DEFAULT_TAB = { ...TABS[0] };
+
+const QUERIES = {
+  bookmarks: BookmarksQuery,
+  likedCocktails: LikedCocktailsQuery,
+  dislikedCocktails: DislikedCocktailsQuery,
+};
+
+export const state = () => ({
+  tabs: [...TABS],
+  currentTab: { ...DEFAULT_TAB },
+  cocktails: {
+    bookmarks: [],
+    likedCocktails: [],
+    dislikedCocktails: [],
+  },
+  meta: {
+    bookmarks: {
+      loading: false,
+      hasNextPage: true,
+      after: null,
+    },
+    likedCocktails: {
+      loading: false,
+      hasNextPage: true,
+      after: null,
+    },
+    dislikedCocktails: {
+      loading: false,
+      hasNextPage: true,
+      after: null,
+    },
+  },
+});
+
+export const mutations = {
+  ...make.mutations(['currentTab', 'cocktails', 'meta']),
+  ADD_COCKTAILS({ cocktails, currentTab }, furtherCocktails) {
+    cocktails[currentTab.key].push(...furtherCocktails);
+  },
+  SET_HAS_NEXT_PAGE({ meta, currentTab }, hasNextPage) {
+    meta[currentTab.key].hasNextPage = hasNextPage;
+  },
+  SET_LOADING({ meta, currentTab }, loading) {
+    meta[currentTab.key].loading = loading;
+  },
+  SET_AFTER({ meta, currentTab }, endCursor) {
+    meta[currentTab.key].after = endCursor;
+  },
+
+  SET_BOOKMARKS({ cocktails }, bookmarks) {
+    cocktails.bookmarks = bookmarks;
+  },
+  SET_LIKED_COCKTAILS({ cocktails }, likedCocktails) {
+    cocktails.likedCocktails = likedCocktails;
+  },
+  SET_DISLIKED_COCKTAILS({ cocktails }, dislikedCocktails) {
+    cocktails.dislikedCocktails = dislikedCocktails;
+  },
+
+  SET_BOOKMARKS_META({ meta }, bookmarksMeta) {
+    meta.bookmarks = bookmarksMeta;
+  },
+  SET_LIKED_COCKTAILS_META({ meta }, likedCocktailsMeta) {
+    meta.likedCocktails = likedCocktailsMeta;
+  },
+  SET_DISLIKED_COCKTAILS_META({ meta }, dislikedCocktailsMeta) {
+    meta.dislikedCocktails = dislikedCocktailsMeta;
+  },
+
+  REMOVE_BOOKMARK({ cocktails }, bookmarkIndex) {
+    cocktails.bookmarks.splice(bookmarkIndex, 1);
+  },
+  REMOVE_LIKED_COCKTAIL({ cocktails }, likeIndex) {
+    cocktails.likedCocktails.splice(likeIndex, 1);
+  },
+  REMOVE_DISLIKED_COCKTAIL({ cocktails }, dislikeIndex) {
+    cocktails.likedCocktails.splice(dislikeIndex, 1);
+  },
+};
+
+export const actions = {
+  ...make.actions(['currentTab', 'cocktails', 'meta']),
+
+  async fetch({ commit, getters }) {
+    const { currentMeta: { hasNextPage, loading, after }, currentTab } = getters;
+    if (!hasNextPage || loading) return;
+
+    const client = this.app.apolloProvider.defaultClient;
+    commit('SET_LOADING', true);
+
+    const query = QUERIES[currentTab.key];
+    const payload = {
+      query,
+      variables: {
+        first: 20,
+        after,
+      },
+    };
+    if (getters.cocktails[currentTab.key].length === 0) payload.fetchPolicy = 'no-cache';
+    const { data } = await client.query(payload);
+
+    commit('SET_HAS_NEXT_PAGE', Object.values(data)[0].pageInfo.hasNextPage);
+    commit('SET_AFTER', Object.values(data)[0].pageInfo.endCursor);
+    commit('ADD_COCKTAILS', Object.values(data)[0].cocktails);
+    commit('SET_LOADING', false);
+  },
+
+  switchTab({ commit, dispatch }, tab) {
+    const newTab = TABS.find((t) => t.key === tab.key);
+
+    commit('SET_CURRENT_TAB', newTab);
+    dispatch('fetch');
+  },
+
+  reset({ commit }) {
+    commit('SET_CURRENT_TAB', { ...DEFAULT_TAB });
+    commit('SET_COCKTAILS', {
+      bookmarks: [],
+      likedCocktails: [],
+      dislikedCocktails: [],
+    });
+    commit('SET_META', {
+      bookmarks: {
+        loading: false,
+        hasNextPage: true,
+        after: null,
+      },
+      likedCocktails: {
+        loading: false,
+        hasNextPage: true,
+        after: null,
+      },
+      dislikedCocktails: {
+        loading: false,
+        hasNextPage: true,
+        after: null,
+      },
+    });
+  },
+
+  resetTab({ commit }, { key: tab }) {
+    if (tab === 'bookmarks') {
+      commit('SET_BOOKMARKS', []);
+      commit('SET_BOOKMARKS_META', {
+        loading: false,
+        hasNextPage: true,
+        after: null,
+      });
+    } else if (tab === 'likedCocktails') {
+      commit('SET_LIKED_COCKTAILS', []);
+      commit('SET_LIKED_COCKTAILS_META', {
+        loading: false,
+        hasNextPage: true,
+        after: null,
+      });
+    } else if (tab === 'dislikedCocktails') {
+      commit('SET_DISLIKED_COCKTAILS', []);
+      commit('SET_DISLIKED_COCKTAILS_META', {
+        loading: false,
+        hasNextPage: true,
+        after: null,
+      });
+    }
+  },
+
+  onAction({ commit, dispatch, getters }, { index: cocktailIndex, action }) {
+    const { currentTab: { key: currentTab } } = getters;
+    if (['dislike', 'unset-review'].includes(action) && currentTab === 'likedCocktails') {
+      commit('REMOVE_LIKED_COCKTAIL', cocktailIndex);
+    } else if (['like', 'unset-review'].includes(action) && currentTab === 'dislikedCocktails') {
+      commit('REMOVE_DISLIKED_COCKTAIL', cocktailIndex);
+    } else if (action === 'remove-bookmark' && currentTab === 'bookmarks') {
+      commit('REMOVE_BOOKMARK', cocktailIndex);
+    }
+
+    TABS.filter((tab) => tab.key !== getters.currentTab.key).forEach((tab) => {
+      dispatch('resetTab', tab);
+    });
+  },
+};
+
+export const getters = {
+  ...make.getters(state),
+
+  currentMeta({ currentTab, meta }) {
+    return meta[currentTab.key];
+  },
+};
